287d2112546e28ec6918165eca333e885b30187b
Wong Rainie <rainiewong@astrocorp.com.tw>
2020-09-30 11:33:51 +0800

    Fix: update

--- temp/quick-scripts/src/assets/script/ItemList.js
+++ temp/quick-scripts/src/assets/script/ItemList.js
@@ -89,11 +89,12 @@ cc.Class({
         } //所以，y軸就是利用計數器乘上圖片的高度，再加上10額外邊界後，再減去場景的高度除以二
 
 
-        var newX = buttonBG.width * root.Count + 1 - CanvasNode.width / 2; //減去場景高度這個動作，是為了讓每點擊一次按鈕，我們的Prefab就一直往上產生
-        //newMyPrefab.setContentSize(200);
+        var newX = buttonBG.width * 0.5 * root.Count - CanvasNode.width / 2 - 60; //減去場景高度這個動作，是為了讓每點擊一次按鈕，我們的Prefab就一直往上產生
+
+        console.log('buttonBG.width', buttonBG.width); //newMyPrefab.setContentSize(200);
         //設定位置，這邊x軸不變
 
-        newMyPrefab.setPosition(newX, -150);
+        newMyPrefab.setPosition(newX, -130);
         newMyPrefab.getComponent('ItemTemplate').init({
           id: item.id,
           itemName: item.itemName,
